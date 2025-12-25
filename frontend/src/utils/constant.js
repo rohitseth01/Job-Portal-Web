4c13d70639c066cb6c211b252b4a145748c5ac64
rohitseth01 <rohitseth010@example.com>
2025-12-25 20:44:08 +0530

    changes

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -4,7 +4,8 @@
 // export const COMPANY_API_END_POINT="http://localhost:8000/api/v1/company";
 // frontend/src/utils/constant.js
 // frontend/src/utils/constant.js
-const BASE_URL = import.meta.env.VITE_BACKEND_URL || "https://job-portal-web-7t1r.onrender.com";
+// frontend/src/utils/constant.js
+const BASE_URL = "https://job-portal-web-7t1r.onrender.com"; 
 
 export const USER_API_END_POINT = `${BASE_URL}/api/v1/user`;
 export const JOB_API_END_POINT = `${BASE_URL}/api/v1/job`;
